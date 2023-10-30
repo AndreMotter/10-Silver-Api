f315dd27d051f9478114503b61800ebcbca5e8b4
André Motter <andremotter09@gmail.com>
2023-10-30 00:07:49 -0300

    ajustes

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,12 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Configura o banco de dados
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     var connectionString = builder.Configuration.GetConnectionString("ApiDb");
     options.UseNpgsql(connectionString);
 });
 
+//Injeção de dependências
 Api.Application.DependencyInjectionConfig.InjectServices(builder.Services);
 Api.Persistense.DependencyInjectionConfig.InjectRepositories(builder.Services);
 
@@ -20,6 +22,14 @@ builder.Services.AddCors();
 builder.Services.AddOptions();
 
 var app = builder.Build();
+
+//Executa Migration
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var dbContext = services.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
